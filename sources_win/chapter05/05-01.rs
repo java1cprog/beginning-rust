d9eff3ede67fc94dd2586b19e4b3dcad6452d8a1
Anatolij <java1cprog@yandex.ru>
2018-09-08 08:40:06 +0200

    create my example

--- sources_win/chapter05/05-01.rs
+++ sources_win/chapter05/05-01.rs
@@ -1,7 +1,16 @@
 /* It prints:
 This is a sentence in English*/
+/*
 fn main() {
     let x = ["English", "This", "sentence", "a", "in", "is"];
     print!("{} {} {} {} {} {}",
         x[1], x[5], x[3], x[2], x[4], x[0]);
+}
+*/
+fn main() {
+    let x = ["English", "This", "sentence", "a", "in", "is"];
+    println!("{} {} {} {} {} {}", x[1], x[5], x[3], x[2], x[4], x[0]);
+
+    let y = ["Hello", ",", " ", "World!"];
+    println!("{}{}{}{}", y[0], y[1], y[2], y[3]);
 }
