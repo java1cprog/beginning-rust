d8d29f7b17b92e90317cb4177f40284a17aaea53
Anatolij <java1cprog@yandex.ru>
2018-09-09 13:45:11 +0200

    show working with an array and a vector

--- sources_win/chapter06/06-12.rs
+++ sources_win/chapter06/06-12.rs
@@ -1,12 +1,25 @@
 // ILLEGAL
 fn main() {
-    let arr = [11, 22, 33];
-    let i: usize = 2;
-    print!("{}", arr[i]);
-    let i: isize = 2;
-    print!("{}", arr[i]);
-    let i: u32 = 2;
-    print!("{}", arr[i]);
-    let i: u64 = 2;
-    print!("{}", arr[i]);
+    {
+        let arr = [11, 22, 33];
+        let i: usize = 2;
+        print!("{}", arr[i]);
+        let i: isize = 2;
+        print!("{}", arr[i]);
+        let i: u32 = 2;
+        print!("{}", arr[i]);
+        let i: u64 = 2;
+        print!("{}", arr[i]);
+    }
+    {
+        let arr = vec![11, 22, 33];
+        let i: usize = 2;
+        print!("{}", arr[i]);
+        let i: isize = 2;
+        print!("{}", arr[i]);
+        let i: u32 = 2;
+        print!("{}", arr[i]);
+        let i: u64 = 2;
+        print!("{}", arr[i]);
+    }
 }
