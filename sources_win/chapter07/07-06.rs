536bd43d03b520ed323891ca4332312c383b2995
Anatolij <java1cprog@yandex.ru>
2018-09-09 19:58:07 +0200

    This example best describes the highlights issues

--- sources_win/chapter07/07-06.rs
+++ sources_win/chapter07/07-06.rs
@@ -8,3 +8,26 @@ fn main() {
         Continent::Oceania => print!("O"),
     }
 }
+/*
+#[allow(dead_code)]
+fn main() {
+    enum Continent {
+        Europe,
+        Asia,
+        Africa,
+        America,
+        Oceania,
+    }
+
+    let contin = Continent::Asia;
+    match contin {
+     //   Continent::Europe => let a = 7;,
+        //Continent::Asia => let a = 7,
+    //    Continent::Africa => fn aaa() {},
+        Continent::America => print!("Am"),
+        Continent::Oceania => print!("O"),
+        _ => {},
+    }
+}
+
+*/
