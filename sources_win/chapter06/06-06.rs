74f20f03f4557ca8461d51e51d58d41cb9beb0ac
Anatolij <java1cprog@yandex.ru>
2018-09-09 11:01:40 +0200

    add println! for printing to console

--- sources_win/chapter06/06-06.rs
+++ sources_win/chapter06/06-06.rs
@@ -4,4 +4,9 @@ fn main() {
     let one_million = 1e6;
     let thirteen_billions_and_half = 13.5e9;
     let twelve_millionths = 12e-6;
+
+    println!(
+        "{} {} {} {}",
+        one_thousand, one_million, thirteen_billions_and_half, twelve_millionths
+    );
 }
