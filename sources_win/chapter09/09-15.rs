cc5575ea0fa584f5ac14a1542ae4ae4f40519248
Anatolij <java1cprog@yandex.ru>
2018-09-10 13:17:26 +0200

    add using macros println!

--- sources_win/chapter09/09-15.rs
+++ sources_win/chapter09/09-15.rs
@@ -1,6 +1,10 @@
-// It does nothing.
+// It prints 73 73 173.
 fn main() {
     fn f1() -> i32 { 4.5; "abc"; 73i32 }
     fn f2() -> i32 { 4.5; "abc"; 73 }
     fn f3() -> i32 { 4.5; "abc"; 73 + 100 }
+
+    println!("{}", f1());
+    println!("{}", f2());
+    println!("{}", f3());
 }
