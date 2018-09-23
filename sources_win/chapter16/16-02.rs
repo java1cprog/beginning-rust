a5b8626eb1322921f2ff1eb71dfd4d129ce489a0
Anatolij <java1cprog@yandex.ru>
2018-09-23 13:30:47 +0200

    fix algorithmic problems

--- sources_win/chapter16/16-02.rs
+++ sources_win/chapter16/16-02.rs
@@ -1,16 +1,29 @@
 /* It prints:
 e*/
-fn main() {
-    fn print_nth_char(s: &str, mut n: u32) {
-        let mut iter: std::str::Chars = s.chars();
-        loop {
-            let item: Option<char> = iter.next();
-            match item {
-                Some(c) => if n == 1 { print!("{}", c); },
-                None => { break; },
+fn print_nth_char(s: &str, n: u32) {
+    let mut iter: std::str::Chars = s.chars();
+    let n_item = n-1;
+    let mut index = 0;
+    loop {
+        let item: Option<char> = iter.next();
+        match item {
+            Some(c) => 
+             if n_item == index
+            {
+                println!("{} ", c);
+            },
+            None => {
+                break;
             }
-            n -= 1;
         }
+        index += 1;
     }
-    print_nth_char("€èe", 3);
 }
+
+fn main() {
+    let string = "€èe";
+    print_nth_char(string, 1);
+    print_nth_char(string, 2);
+    print_nth_char(string, 3);
+}
+
