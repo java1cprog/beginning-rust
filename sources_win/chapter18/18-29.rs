8609ad58e343eac4f2a782cc2de887dc6e479d73
Anatolij <java1cprog@yandex.ru>
2018-09-27 07:36:10 +0200

    add #[allow(dead_code)]

--- sources_win/chapter18/18-29.rs
+++ sources_win/chapter18/18-29.rs
@@ -1,10 +1,13 @@
 // It does nothing.
+
 fn main() {
     trait Searchable<Key> {
         fn contains(&self, key: Key) -> bool;
     }
+    #[allow(dead_code)]
     fn is_present<Collection>(coll: &Collection, id: u32) -> bool
-    where Collection: Searchable<u32>
+    where
+        Collection: Searchable<u32>,
     {
         coll.contains(id)
     }
